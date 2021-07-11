906595854fce57d2df037c21236750f669d52c80
Liesbeth-P <71521815+Liesbeth-P@users.noreply.github.com>
2021-07-11 19:03:20 +0200

    Javascript

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $('.hamburger-button').click(function(){
+        $('.mobile-menu').fadeToggle();
+        $('this').toggleClass('active');
+      });
+
+    //   Animate on Scroll - Activate
+    AOS.init({
+        duration: 500 // values from 0 to 3000, with step 50ms
+    });
+    
+});
