f6932a84eb79e607f53e815a85f94659a27175b6
Liesbeth-P <71521815+Liesbeth-P@users.noreply.github.com>
2021-08-01 13:42:10 +0200

    Js scripts

--- scripts.js
+++ scripts.js
@@ -9,13 +9,13 @@ $( document ).ready(function() {
     AOS.init({
         duration: 500 // values from 0 to 3000, with step 50ms
     });
-    
 });
 
 lightGallery(document.getElementById('lightgallery'), {
     speed: 500,
     download: false
 });
+
 
 // lightGallery(document.getElementById('lightgallery-2'), {
 //     speed: 500,
